55704539c5ae38a0dbc860e222eeaa56d950b775
Tim <tim@brightsidedeveloper.com>
2023-02-26 22:08:14 -0600

    fallback

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-import React from "react"
+import React, { Suspense } from "react"
 import ReactDOM from "react-dom/client"
 import "./index.css"
 import router from "./Router"
 import { RouterProvider } from "react-router-dom"
+import Loading from "./components/Loading"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Suspense
+      fallback={
+        <div className="w-screen h-screen flex justify-center items-center">
+          <Loading />
+        </div>
+      }
+    >
+      <RouterProvider router={router} />
+    </Suspense>
   </React.StrictMode>
 )
